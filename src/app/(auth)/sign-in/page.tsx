dc1fcbc881f3700e4ea6810cd029ec56edea1698
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2025-01-28 10:20:21 -0800

    Delete src directory

--- src/app/(auth)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs';
-
-export default function SignInPage() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <SignIn />
-    </div>
-  );
-}
--- src/lib/helper/shipEngine.ts
+++
@@ -1,5 +0,0 @@
-import ShipEngine from "shipengine";
-
-export const shipengine = new ShipEngine({
-    apiKey: process.env.SHIPENGINE_API_KEY as string,
-  });
