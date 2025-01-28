8e0b7318fabaab7e2d05677e274ef11f89c8268a
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2025-01-28 10:03:57 -0800

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,36 @@
+// next.config.js
+const nextConfig = {
+  images: {
+    domains: [
+      "www.secretsales.com", // Already added
+      "media.secretsales.com", // Already added
+      "static.nike.com", // Already added
+      "encrypted-tbn0.gstatic.com", // Already added
+      "www.nike.com.kw", // Already added
+      "www.nike.sa", // Add this domain
+     "www.thenextsole.com",
+     "cdn.shopify.com",
+     "atmos-kl.com",
+     "media.jdsports.com",
+     "images.clerk.dev",
+     "images.puma.com",
+     "reebok.bynder.com",
+     "i.ebayimg.com",
+      "www.nike.ae",
+      "speedsports.pk",
+       "www.jdsports.cy",
+       "www.buymo.co.za",
+        "stndrdnola.com",
+        "konfor.pk",
+        "dks.scene7.com",
+        "images.bike24.com",
+        "www.alay.co",
+        "c-hackathon-2-dec.vercel.app"
+        
+
+
+    ],
+  },
+};
+
+module.exports = nextConfig;
